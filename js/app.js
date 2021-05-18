f4f8f054840b19236b05b0d4c5965a5bc7856664
simshadows <contact@simshadows.com>
2021-05-19 01:01:59 +1000

    Webpage now renders three empty boxes.

--- js/app.js
+++ js/app.js
@@ -1,9 +1,99 @@
-const elem = React.createElement;
+/*
+ * Author:  simshadows <contact@simshadows.com>
+ * License: GNU Affero General Public License v3 (AGPL-3.0)
+ */
 
-import HelloWorldComponent from "./components/HelloWorldComponent.js";
+const element = React.createElement;
+
+/*********************************************************************
+ * Model *************************************************************
+ *********************************************************************/
+
+//class Build {
+//
+//    constructor() {
+//        // TODO
+//    }
+//
+//    getText() {
+//        return "Hello, world!";
+//    }
+//
+//}
+
+/*********************************************************************
+ * Rendering: Components *********************************************
+ *********************************************************************/
+
+function SkillsResultsBox() {
+    return element("div",
+        {
+        id: "skillsresultsbox",
+        className: "mainbox",
+        },
+        element("p",
+            null,
+            "SkillsResultsBox",
+        ),
+    );
+}
+
+function EquipmentSelectionsBox() {
+    return element("div",
+        {
+        id: "equipmentselectionsbox",
+        className: "mainbox",
+        },
+        element("p",
+            null,
+            "EquipmentSelectionsBox",
+        ),
+    );
+}
+
+function CalculationResultsBox() {
+    return element("div",
+        {
+        id: "calculationresultsbox",
+        className: "mainbox",
+        },
+        element("p",
+            null,
+            "CalculationResultsBox",
+        ),
+    );
+}
+
+class MHRBuilderAppContainer extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {message: "kek"};
+    }
+
+    render() {
+        return element("div",
+            {
+            className: "tmpboxes mhrbuilderappcontainer",
+            },
+            element(SkillsResultsBox,
+                null,
+                null,
+            ),
+            element(EquipmentSelectionsBox,
+                null,
+                null,
+            ),
+            element(CalculationResultsBox,
+                null,
+                null,
+            ),
+        );
+    }
+}
 
 ReactDOM.render(
-    elem("div", null, elem(HelloWorldComponent, null, null)),
+    element(MHRBuilderAppContainer, null),
     document.getElementById("mhr-builder-app-container")
 );
 
