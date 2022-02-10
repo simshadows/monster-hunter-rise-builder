27de53513a1b11dc9911764246f86562c2a57ad1
simshadows <contact@simshadows.com>
2022-02-10 18:28:58 +1100

    Changed FrozenMap.#underlying to the TypeScript private syntax
    This makes it so the underlying map is still visible in Google Chrome DevTools. This should be changed back to #underlying if DevTools ever allows you to inspect #-syntax private fields.

--- src/mhrb/_assets/generic/frozen-containers.ts
+++ src/mhrb/_assets/generic/frozen-containers.ts
@@ -4,36 +4,36 @@
  */
 
 export class FrozenMap<K, V> {
-    readonly #underlying: Readonly<Map<K, V>>;
+    private readonly _underlying: Readonly<Map<K, V>>;
 
     // TODO: How do I get something like this to work?
     //constructor(...args: ConstructorParameters<Map<K, V>>) {
-    //    this.#underlying = new Map<K, V>(...args);
+    //    this._underlying = new Map<K, V>(...args);
     //}
 
     // TODO: Highly constrained for now since I'm not sure how to forward the arguments without losing type safety.
     constructor(toBeFrozen: Map<K, V>) {
-        this.#underlying = new Map<K, V>(toBeFrozen);
+        this._underlying = new Map<K, V>(toBeFrozen);
     }
 
     // TODO: Rather than define each method individually, why not generate them?
     get(...args: Parameters<Map<K, V>["get"]>): ReturnType<Map<K, V>["get"]> {
-        return this.#underlying.get(...args);
+        return this._underlying.get(...args);
     }
     has(...args: Parameters<Map<K, V>["has"]>): ReturnType<Map<K, V>["has"]> {
-        return this.#underlying.has(...args);
+        return this._underlying.has(...args);
     }
     keys(...args: Parameters<Map<K, V>["keys"]>): ReturnType<Map<K, V>["keys"]> {
-        return this.#underlying.keys(...args);
+        return this._underlying.keys(...args);
     }
     values(...args: Parameters<Map<K, V>["values"]>): ReturnType<Map<K, V>["values"]> {
-        return this.#underlying.values(...args);
+        return this._underlying.values(...args);
     }
     entries(...args: Parameters<Map<K, V>["entries"]>): ReturnType<Map<K, V>["entries"]> {
-        return this.#underlying.entries(...args);
+        return this._underlying.entries(...args);
     }
     get size(): Map<K, V>["size"] {
-        return this.#underlying.size;
+        return this._underlying.size;
     }
 }
 
